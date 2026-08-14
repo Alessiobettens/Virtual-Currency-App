7b2285c0dc8257e5e28d353ec2d0ccada695d3f1
Alessiobettens <alessio.bettens@student.ehb.be>
2026-08-15 00:29:59 +0200

    git add . git commit -m "Add user registration with bcrypt password hashing" git push

--- register.php
+++ register.php
@@ -1,15 +1,83 @@
-$user = new User();
+<?php
 
-$user->setFullname($_POST['fullname']);
-$user->setEmail($_POST['email']);
+require_once 'classes/User.php';
 
-$hashedPassword = password_hash(
-    $_POST['password'],
-    PASSWORD_BCRYPT
-);
+$message = "";
 
-$user->setPassword($hashedPassword);
+if (!empty($_POST)) {
 
-$user->setBalance(10);
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-$user->save();
+    // Controle email
+    if (!str_ends_with($email, '@student.thomasmore.be')) {
+        $message = "Gebruik een ThomasMore e-mailadres.";
+    }
+
+    // Controle wachtwoord
+    elseif (strlen($password) < 5) {
+        $message = "Wachtwoord moet minstens 5 karakters hebben.";
+    }
+
+    else {
+
+        $user = new User();
+
+        $user->setFullname($fullname);
+        $user->setEmail($email);
+
+        $hashedPassword = password_hash(
+            $password,
+            PASSWORD_BCRYPT
+        );
+
+        $user->setPassword($hashedPassword);
+        $user->setBalance(10);
+
+        $user->save();
+
+        $message = "Registratie gelukt!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registreren</title>
+</head>
+<body>
+
+<h1>Registreren</h1>
+
+<p><?php echo $message; ?></p>
+
+<form method="post">
+
+    <label>Volledige naam</label>
+    <br>
+    <input type="text" name="fullname" required>
+
+    <br><br>
+
+    <label>Email</label>
+    <br>
+    <input type="email" name="email" required>
+
+    <br><br>
+
+    <label>Wachtwoord</label>
+    <br>
+    <input type="password" name="password" required>
+
+    <br><br>
+
+    <button type="submit">
+        Registreren
+    </button>
+
+</form>
+
+</body>
+</html>
