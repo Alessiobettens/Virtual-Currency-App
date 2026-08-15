6857d97cec54b17e3af81bb0ca130330f5a81a37
Alessiobettens <alessio.bettens@student.ehb.be>
2026-08-16 00:34:39 +0200

    Add user info to dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,10 +2,32 @@
 
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
+require_once 'classes/User.php';
 
+if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
 }
 
-echo "Je bent ingelogd!";
+$user = User::getById($_SESSION['user_id']);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<h1>Welkom <?php echo htmlspecialchars($user['fullname']); ?></h1>
+
+<p>
+    Saldo:
+    <?php echo htmlspecialchars($user['balance']); ?>
+    tokens
+</p>
+
+<a href="logout.php">Uitloggen</a>
+
+</body>
+</html>
