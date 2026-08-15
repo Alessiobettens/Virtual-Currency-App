4634450db14dbb954e3683fe82415657f86049c2
Alessiobettens <alessio.bettens@student.ehb.be>
2026-08-15 23:44:45 +0200

    Add authentication with password verification

--- login.php
+++ login.php
@@ -1,9 +1,35 @@
+<?php
+
+session_start();
+
+require_once 'classes/User.php';
+
+$message = "";
+
+if (!empty($_POST)) {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = User::findByEmail($email);
+
+    if ($user && password_verify($password, $user['password'])) {
+
+        $_SESSION['user_id'] = $user['id'];
+
+        echo "Login gelukt!";
+    } else {
+        $message = "Email of wachtwoord is fout.";
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Login</title>
 </head>
 <body>
+    <p><?php echo htmlspecialchars($message); ?></p>
 
 <h1>Login</h1>
 
