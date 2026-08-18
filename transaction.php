e1b80b20ef3b45f10b14307390d72fc4df3d51cd
Alessiobettens <alessio.bettens@student.ehb.be>
2026-08-18 14:23:55 +0200

    add styles.css to transaction

--- transaction.php
+++ transaction.php
@@ -7,11 +7,16 @@ $id = $_GET['id'];
 $transaction = Transaction::getById($id);
 
 ?>
-
+<link rel="stylesheet" type="text/css" href="assets/style.css">
+<div class="container">
+    <div class="card">
 <h1>Transaction Details</h1>
 
-<p>ID: <?php echo $transaction['id']; ?></p>
-<p>Sender: <?php echo $transaction['sender_id']; ?></p>
-<p>Receiver: <?php echo $transaction['receiver_id']; ?></p>
-<p>Amount: <?php echo $transaction['amount']; ?></p>
-<p>Message: <?php echo htmlspecialchars($transaction['message']); ?></p>
+        <p>ID: <?php echo $transaction['id']; ?></p>
+        <p>Sender: <?php echo $transaction['sender_id']; ?></p>
+        <p>Receiver: <?php echo $transaction['receiver_id']; ?></p>
+        <p>Amount: <?php echo $transaction['amount']; ?></p>
+        <p>Message: <?php echo htmlspecialchars($transaction['message']); ?></p>
+    
+    </div>
+</div>
